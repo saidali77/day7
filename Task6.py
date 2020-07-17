8e6295b33e8d1d24fb076f9fcb14c0ebd62e21c9
saidali77 <mamedovsaidali7@gmail.com>
2020-07-17 16:33:14 +0600

    test7

---
+++ Task6.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+print('Введите 1-ое значение')
+user1 = int(input())
+
+print('Введите 2-ое значение')
+user2 = int(input())
+
+ if user1 + user2 > 5:
+     print('Сумма больше 5')
+else:
+    print('Сумма меньше 5')     
